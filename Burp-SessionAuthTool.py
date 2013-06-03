5405095dac6215ed48b8f4321dfd1607bf300b0d
Thomas Skora <thomas@skora.net>
2013-06-03 22:57:03 +0200

    Added base request/response to scan issues. Added copyright and GPLv3 notice.

--- Burp-SessionAuthTool.py
+++ Burp-SessionAuthTool.py
@@ -1,6 +1,18 @@
-# This Burp extensions provides passive and active scanner checks for
-# detection of potential privilege escalation issues caused by
-# transmission of user identifiers from the client.
+# Burp SessionAuthTool Extension
+# Copyright 2013 Thomas Skora <thomas@skora.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from burp import (IBurpExtender, ITab, IScannerCheck, IScanIssue, IContextMenuFactory, IContextMenuInvocation, IParameter, IIntruderPayloadGeneratorFactory, IIntruderPayloadGenerator)
 from javax.swing import (JPanel, JTable, JButton, JTextField, JLabel, JScrollPane, JMenuItem)
@@ -458,7 +470,7 @@ class SessionAuthActiveScanIssue(IScanIssue):
         to access."
 
     def getHttpMessages(self):
-        return [self.scanRequestResponse]
+        return [self.baseRequestResponse, self.scanRequestResponse]
 
     def getHttpService(self):
         return self.service
@@ -548,7 +560,7 @@ class MappingTableModel(AbstractTableModel):
             self.mappings[self.idorder[row]] = val
             self.fireTableCellUpdated(row, col)
         self.saveMapping()
-        
+
     def getIds(self):
         return self.idorder
 
